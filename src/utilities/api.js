99face17ff303a2738b61ecb819df99e71f7696b
Adithya kamath <kamathadithya12@gmail.com>
2024-01-13 19:39:08 +0530

    addiing api url fix https

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -2,7 +2,7 @@ export const YT_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&maxResults=50&chart=mostPopular&regionCode=IN&";
 
 export const YT_SEARCH =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const YT_SEARCH_API =
   "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=";
